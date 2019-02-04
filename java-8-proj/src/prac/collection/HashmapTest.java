cf8af114d3f34ad51e8aaebf98fd6481504104dd
Ranjan Kalita <ranjankalita@bitbucket.org>
2019-02-04 13:03:09 +0530

    java 8 first comit

---
+++ java-8-proj/src/prac/collection/HashmapTest.java
@@ -0,0 +1,12 @@
+package prac.collection;
+
+import java.util.HashMap;
+
+public class HashmapTest {
+
+	public static void main(String[] args) {
+		HashMap<String, String> hm = new HashMap<String, String>();
+
+	}
+
+}
