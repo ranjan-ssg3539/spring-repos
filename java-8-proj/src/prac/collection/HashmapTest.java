4f2cac27ed1b0d9ad7ea764309f327804a8bfe0c
ranjan <ranjankalita@bitbucket.org>
2019-02-12 18:47:41 +0530

    just do it

--- java-8-proj/src/prac/collection/HashmapTest.java
+++ java-8-proj/src/prac/collection/HashmapTest.java
@@ -1,12 +1,33 @@
 package prac.collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.omg.Messaging.SyncScopeHelper;
 
 public class HashmapTest {
 
 	public static void main(String[] args) {
-		HashMap<String, String> hm = new HashMap<String, String>();
-
+		Map<String, Integer> hm = new HashMap<String, Integer>();
+		hm.put("a", 1);
+		hm.put("d", 3);
+		hm.put("b", 4);
+		hm.put("c", 2);
+		
+		List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>();
+		Collections.sort(list, new Comparator<Entry<String, Integer>>());
+		
+		
+		Collections.sy
 	}
 
 }
